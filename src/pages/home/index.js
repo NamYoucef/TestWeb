805d7746fa7280bd2c826738a5c47c93a6a61471
NamYoucef <98276877+NamYoucef@users.noreply.github.com>
2022-03-28 11:47:54 +0100

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,49 +1,18 @@
 import "./style.css";
-import { CheckOutlined } from "@ant-design/icons";
-import "antd/dist/antd.css";
-import { List } from "antd";
+import useFetch from "../../api/useFetch";
 function Home() {
-  const data = [
-    {
-      title: "Title1 ",
-      description: "description1",
-      date: "date1",
-    },
-    {
-      title: "Title2",
-      description: "description2",
-      date: "date2",
-    },
-    {
-      title: "Title3",
-      description: "description3",
-      date: "date3",
-    },
-    {
-      title: "Title4",
-      description: "description4",
-      date: "date4",
-    },
-  ];
+  const { donnee } = useFetch("https://backtest01.herokuapp.com/");
   return (
     <>
       <div className="container">
         <h1 className="title">Todo list</h1>
-        <List
-          itemLayout="horizontal"
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<CheckOutlined />}
-                title={item.title}
-                description={item.description}
-                date={item.date}
-              />
-            </List.Item>
-          )}
-        />
-        ,
+        <ul>
+          {donnee.map((value, index) => (
+            <li key={index}>
+              {value.title} {value.description} {value.date}{" "}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
