5f065a73d7b0cb3afab4224f8e5a67e8eef803cd
NamYoucef <98276877+NamYoucef@users.noreply.github.com>
2022-03-28 11:14:55 +0100

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import useFetch from "../../api/useFetch";
-import { useLocation } from "react-router";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 import "./style.css";
 
